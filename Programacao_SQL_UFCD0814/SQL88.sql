6a89c58fbb7dca6f20f0f726eed599a265c8997b
Manoel Barata <106320376+msbarata@users.noreply.github.com>
2026-04-04 20:36:56 +0100

    Delete Programacao_SQL_UFCD0814/SQL88.sql

--- Programacao_SQL_UFCD0814/SQL88.sql
+++
@@ -1,4 +0,0 @@
-SELECT Sigla_Hotel, COUNT(DISTINCT Numero_Reserva) as "reservas", RANK() OVER ( ORDER BY reservas DESC) FROM Reserva
-JOIN Reserva_Quartos
-USING (Numero_Reserva)
-GROUP BY Sigla_Hotel;
